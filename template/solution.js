7f31ce1c6f444879ba4bd51032c3f277733f99e8
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2023-12-11 15:55:08 +0800

    Add template

---
+++ template/solution.js
@@ -0,0 +1,13 @@
+import fs from 'fs/promises';
+
+const test1 = ``;
+
+function solution1(input) {
+}
+
+function solution2(input) {
+}
+
+const result = await fs.readFile('./inputs.txt', 'utf-8');
+
+console.log(solution2(result));
